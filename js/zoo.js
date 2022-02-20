e0fcbf6ff81b79475150ba8acda47541c9b6a770
KhaiCuong <88977306+KhaiCuong@users.noreply.github.com>
2022-02-20 19:09:15 +0700

    Cuong

---
+++ js/zoo.js
@@ -0,0 +1,73 @@
+
+// TOP RATED SAFARI CONTRIES
+
+function change1() {
+    document.getElementById('tour-info1').style.display = 'block'
+    document.getElementById('tour-info2').style.display = 'none'
+    document.getElementById('tour-info3').style.display = 'none'
+    document.getElementById('tour-info4').style.display = 'none'
+    document.getElementById('tour-info5').style.display = 'none'
+    document.getElementById('tour-info6').style.display = 'none'
+    document.getElementById('tour-info7').style.display = 'none'
+}
+function change2() {
+    document.getElementById('tour-info1').style.display = 'none'
+    document.getElementById('tour-info2').style.display = 'block'
+    document.getElementById('tour-info3').style.display = 'none'
+    document.getElementById('tour-info4').style.display = 'none'
+    document.getElementById('tour-info5').style.display = 'none'
+    document.getElementById('tour-info6').style.display = 'none'
+    document.getElementById('tour-info7').style.display = 'none'
+}
+function change3() {
+    document.getElementById('tour-info1').style.display = 'none'
+    document.getElementById('tour-info2').style.display = 'none'
+    document.getElementById('tour-info3').style.display = 'block'
+    document.getElementById('tour-info4').style.display = 'none'
+    document.getElementById('tour-info5').style.display = 'none'
+    document.getElementById('tour-info6').style.display = 'none'
+    document.getElementById('tour-info7').style.display = 'none'
+}
+function change4() {
+    document.getElementById('tour-info1').style.display = 'none'
+    document.getElementById('tour-info2').style.display = 'none'
+    document.getElementById('tour-info3').style.display = 'none'
+    document.getElementById('tour-info4').style.display = 'block'
+    document.getElementById('tour-info5').style.display = 'none'
+    document.getElementById('tour-info6').style.display = 'none'
+    document.getElementById('tour-info7').style.display = 'none'
+}
+function change5() {
+    document.getElementById('tour-info1').style.display = 'none'
+    document.getElementById('tour-info2').style.display = 'none'
+    document.getElementById('tour-info3').style.display = 'none'
+    document.getElementById('tour-info4').style.display = 'none'
+    document.getElementById('tour-info5').style.display = 'block'
+    document.getElementById('tour-info6').style.display = 'none'
+    document.getElementById('tour-info7').style.display = 'none'
+}
+function change6() {
+    document.getElementById('tour-info1').style.display = 'none'
+    document.getElementById('tour-info2').style.display = 'none'
+    document.getElementById('tour-info3').style.display = 'none'
+    document.getElementById('tour-info4').style.display = 'none'
+    document.getElementById('tour-info5').style.display = 'none'
+    document.getElementById('tour-info6').style.display = 'block'
+    document.getElementById('tour-info7').style.display = 'none'
+}
+function change7() {
+    document.getElementById('tour-info1').style.display = 'none'
+    document.getElementById('tour-info2').style.display = 'none'
+    document.getElementById('tour-info3').style.display = 'none'
+    document.getElementById('tour-info4').style.display = 'none'
+    document.getElementById('tour-info5').style.display = 'none'
+    document.getElementById('tour-info6').style.display = 'none'
+    document.getElementById('tour-info7').style.display = 'block'
+}
+
+
+
+
+
+
+
